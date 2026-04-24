ef617bb6b79a8366dce812c00954b3675be5f965
Kyoiua <nubnubgaming21@gmail.com>
2026-04-24 08:00:40 -0500

    Update api-constants.js

--- src/constants/api-constants.js
+++ src/constants/api-constants.js
@@ -1,19 +1,32 @@
 export const ANILIST_URL = 'https://graphql.anilist.co';
+
 export const ANILIST_QUERY = `
   query ($id: Int) {
     Media(id: $id, type: ANIME) {
       id
+
       title {
         romaji
         english
         native
         userPreferred
       }
-      episodes
+
       synonyms
+      episodes
+      format
+      status
+
+      season
+      seasonYear
+
+      startDate {
+        year
+      }
     }
   }
 `;
+
 export const HIANIME_URL = 'https://hianime.to';
 export const ANIZIP_URL = 'https://api.ani.zip/mappings';
 
@@ -22,4 +35,4 @@ export default {
   ANILIST_QUERY,
   HIANIME_URL,
   ANIZIP_URL
-}; 
+};
